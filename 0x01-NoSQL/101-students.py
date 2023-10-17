495f2c7f2dbc24702e5c87f61162cdb52c4074e3
Toxylee <toxylee20021@gmail.com>
2023-10-17 18:35:50 +0000

    NoSQL

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''Module: Task 101
+'''
+
+
+def top_students(mongo_collection):
+    '''prints all students in a collection sorted by average score.
+    '''
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
